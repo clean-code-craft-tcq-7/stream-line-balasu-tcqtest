b0a3c4df0e42b3dea3216fa3403f3e0e391291a6
Raghavan Vijayakumar <Vijayakumar.Raghavan@in.bosch.com>
2023-02-27 12:28:12 +0530

    newreceiver

---
+++ receiver/receiverinjava/TextfileInput.java
@@ -0,0 +1,8 @@
+package receiverinjava;
+
+
+public class TextfileInput {
+
+	public static String TempInput_filePath = "stream-line-balasu-tcqtest/Sender/TempSensor.txt";
+	public static String SOCInput_filePath = "stream-line-balasu-tcqtest/Sender/SOCSensor.txt";
+}
