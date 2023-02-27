7fd23918964637ece9f16d7393f290e3eefb7727
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2023-02-27 11:59:28 +0530

    Delete src/main/java/receiver directory

--- src/main/java/receiver/TextfileInput.java
+++
@@ -1,8 +0,0 @@
-package receiver;
-
-
-public class TextfileInput {
-
-	static String TempInput_filePath = "stream-line-balasu-tcqtest/Sender/TempSensor.txt";
-	static String SOCInput_filePath = "stream-line-balasu-tcqtest/Sender/SOCSensor.txt";
-}
