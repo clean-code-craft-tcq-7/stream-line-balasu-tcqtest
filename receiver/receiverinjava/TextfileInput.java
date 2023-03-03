4ce0767d4d59e2b5e5ed19854426f34d5694ebce
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2023-03-03 09:58:45 +0530

    Update TextfileInput.java

--- receiver/receiverinjava/TextfileInput.java
+++ receiver/receiverinjava/TextfileInput.java
@@ -3,6 +3,6 @@ package receiverinjava;
 
 public class TextfileInput {
 
-	public static String TempInput_filePath = "stream-line-balasu-tcqtest/Sender/TempSensor.txt";
-	public static String SOCInput_filePath = "stream-line-balasu-tcqtest/Sender/SOCSensor.txt";
+	public static String TempInput_filePath = "Sender/TempSensor.txt";
+	public static String SOCInput_filePath = "Sender/SOCSensor.txt";
 }
